b7bf97b9235e648d3daf0707befe397b67741936
Valeria Karimova <valeriakarimova2912@gmail.com>
2022-03-18 19:57:44 +0300

    feat: create router

--- trial/src/app/app.component.ts
+++ trial/src/app/app.component.ts
@@ -2,7 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<div class="container">
+    <ul class="nav nav-tabs">
+      <li class="nav-item">
+        <a class="nav-link active" routerLink="">Income</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" routerLink="/outcome">Outcome</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link"  routerLink="/loans">Loans</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link"  routerLink="/investments">Investments</a>
+      </li>
+    </ul>
+    <router-outlet></router-outlet>
+  </div>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
--- trial/src/app/app.module.ts
+++ trial/src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { AppRoutingModule } from './app-routing.module';
+import { Routes, RouterModule } from '@angular/router'
 import { AppComponent } from './app.component';
+import { IncomeComponent } from './income/income.component';
+import { OutcomeComponent } from './outcome/outcome.component';
+import { LoansComponent } from './loans/loans.component';
+import { InvestmentsComponent } from './investments/investments.component';
+
+
+const appRoutes: Routes = [
+  {path: '', component: IncomeComponent},
+  {path: 'outcome', component: OutcomeComponent},
+  {path: 'loans', component: LoansComponent},
+  {path: 'investments', component: InvestmentsComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IncomeComponent,
+    OutcomeComponent,
+    LoansComponent,
+    InvestmentsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(appRoutes),
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
